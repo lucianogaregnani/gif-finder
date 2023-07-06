fff8c5d8749cc19472ee0c64e085686285e417c5
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2023-07-05 23:08:24 -0300

    nuevos cambios

--- src/components/gif/GifCard.jsx
+++ src/components/gif/GifCard.jsx
@@ -1,5 +1,5 @@
 import ButtonCard from "../decoration/ButtonCard"
-
+import {AiOutlineCloseSquare} from 'react-icons/ai'
 export default function GifCard({info, removeGif, addGifFav}) {
     
     const {id, url, title, username} = info
@@ -8,7 +8,7 @@ export default function GifCard({info, removeGif, addGifFav}) {
         <div className="fixed h-screen bg-black bg-opacity-75 top-0 w-full flex justify-center items-center z-30">
             <div className="relative max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <button className="w-10 h-10 absolute top-3 right-3" onClick={removeGif} >
-                    <img src="/src/assets/close.png" alt="" />
+                    <AiOutlineCloseSquare className="text-red-500 text-5xl"/>
                 </button>
                 <img className="rounded-t-lg h-72 w-full" src={url} alt="" />
                 <div className="p-5">
